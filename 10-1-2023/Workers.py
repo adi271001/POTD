a7f84bc62e333e5839213ad6807bb2cc1ecc9430
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-10 13:37:17 +0530

    Create Workers.py

---
+++ 10-1-2023/Workers.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = [int(x) for x in input().split()] 
+b = [int(x) for x in input().split()] 
+d = {1: 999999, 2: 999999, 3: 999999}
+for i in range(n):
+    if b[i] == 1:
+        d[1] = min([a[i], d[1]])
+    elif b[i] == 2:
+        d[2] = min([a[i], d[2]])
+    else:
+        d[3] = min([a[i], d[3]])
+print(min([d[1]+d[2],d[3]]))

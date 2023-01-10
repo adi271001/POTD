cc56adbbd3ba1b6dbfe0ff2385bdc897d69c0913
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-10 15:28:16 +0530

    Create Average Array.py

---
+++ 10-1-2023/Average Array.py
@@ -0,0 +1,16 @@
+for i in range(int(input())):
+    n, x = map(int, input().split())
+    a = []
+    if n%2==0:
+        initial = x-n//2
+        k = initial
+        while k < x:
+            a += [k]
+            k+=1
+        k+=1
+        while k <= x+n//2:
+            a += [k]
+            k+=1
+    else:
+        a+= range(x-n//2, x+n//2+1)
+    print(*a)

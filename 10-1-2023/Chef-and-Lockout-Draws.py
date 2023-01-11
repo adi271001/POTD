fbd52fce2e38189ebd99f9b77ca49506ac60ece7
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-11 11:51:10 +0530

    Delete 10-1-2023 directory

--- 10-1-2023/Chef-and-Lockout-Draws.py
+++
@@ -1,7 +0,0 @@
-t = int(input())
-for i in range(t):
-    a,b,c = map(int,input().split(" "))
-    if a+b+c==2*max(a,b,c):
-        print("yes")
-    else:
-        print("no")
--- 10-1-2023/Odd-Repeat.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for _ in range(T):
-    [n, k, s] = map(int, input().split())
-    elements = n + k -1
-    sum_of_n_odd = n**2
-    a = abs(s - sum_of_n_odd)//(k-1)
-    print(a)

c24202f260e92f53985b01d5736b18e2c27087f7
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-12 12:28:36 +0530

    Create Chef-Plays-Ludo.py

---
+++ Codechef/Starters-73/Chef-Plays-Ludo.py
@@ -0,0 +1,10 @@
+Question Link: https://www.codechef.com/problems/LUDO
+    
+t=int(input())
+while(t>0):
+    p=int(input())
+    if(p==6):
+        print("yes")
+    else:
+        print("no")
+    t=t-1

35a8c0daedfae541a0a93d2de792b7419c070147
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-14 00:04:24 +0530

    Create Mario-And-Transformation.py

---
+++ Codechef/13-1-2023/Mario-And-Transformation.py
@@ -0,0 +1,8 @@
+Question Link: https://www.codechef.com/problems/TRANSFORM
+    
+for _ in range (int(input())):
+    l=['huge','small','normal']
+    n=int(input())
+    d=n%3
+    print(l[d-1])
+    

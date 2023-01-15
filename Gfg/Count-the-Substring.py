22b39e5ec15f02aea4f85cdb79a8fe1f4065c3c5
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-15 15:26:07 +0530

    Create Count-the-Substring.py

---
+++ Gfg/Count-the-Substring.py
@@ -0,0 +1,16 @@
+Question Link: https://practice.geeksforgeeks.org/problems/f72994353d123b925ff20f0694b662191df03ea2/1
+    
+from collections import defaultdict
+class Solution():
+    def countSubstring(self, S):
+        acc, le, carry = 0, defaultdict(lambda:0), defaultdict(lambda:0)
+        le[0] = 1; carry[1] = 1
+        ans = 0
+        for c in S:
+            v = 1 if c=='1' else -1
+            acc += v
+            le[acc] += 1 + carry[acc]
+            carry[acc+1] += 1 + carry[acc]
+            carry[acc] = 0
+            ans += le[acc-1]
+        return ans

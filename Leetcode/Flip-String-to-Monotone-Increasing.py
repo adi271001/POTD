7e03a06605bd28010796596e5bfa99311e5ea55a
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-17 11:51:48 +0530

    Create Flip-String-to-Monotone-Increasing.py

---
+++ Leetcode/Flip-String-to-Monotone-Increasing.py
@@ -0,0 +1,11 @@
+Question Link: https://leetcode.com/problems/flip-string-to-monotone-increasing/description/
+    
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        ones, ans = 0, 0                   
+        for num in s:                                                                    
+            if num =='1': ones += 1                                                  
+            elif ones:                      
+                ones -= 1                  
+                ans += 1                                                              
+        return ans

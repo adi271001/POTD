49c43c8bc2fe022be4d6ee81efc81ab59084f260
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-20 00:20:43 +0530

    Create Time-Complexity.py

---
+++ Codechef/19-1-2023/Time-Complexity.py
@@ -0,0 +1,8 @@
+Question Link: https://www.codechef.com/problems/COMPLEXITY
+    
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if (x > y):
+        print("YES")
+    else:
+        print("NO")

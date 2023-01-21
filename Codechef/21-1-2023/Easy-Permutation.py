d33d3d5ed686ec78e8b5e46ba9f20959dc70f0ea
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-21 12:32:19 +0530

    Create Easy-Permutation.py

---
+++ Codechef/21-1-2023/Easy-Permutation.py
@@ -0,0 +1,7 @@
+Question Link: https://www.codechef.com/problems/EASYPERM
+    
+for __ in range(int(input())):
+        l=int(input())
+        k=l+1
+        for m in range(1,l+1):
+            print(k-m,end=' ')

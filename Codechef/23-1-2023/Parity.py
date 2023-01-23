7bb55d962ff1fbf59e79a3637cf4209683628b84
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-24 00:31:26 +0530

    Create Parity.py

---
+++ Codechef/23-1-2023/Parity.py
@@ -0,0 +1,9 @@
+Question Link: https://www.codechef.com/problems/PAR2
+    
+p=int(input())
+for _ in range(p):
+    k=int(input())
+    if(k%2==0):
+        print("YES")
+    else:
+        print("NO")

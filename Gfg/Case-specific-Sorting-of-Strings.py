91b58ee9fb93dd02c2860e60d216212aa75476fc
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-26 12:44:42 +0530

    Update Case-specific-Sorting-of-Strings.py

--- Gfg/Case-specific-Sorting-of-Strings.py
+++ Gfg/Case-specific-Sorting-of-Strings.py
@@ -1,1 +1,21 @@
 Question Link: https://practice.geeksforgeeks.org/problems/case-specific-sorting-of-strings4845/1
+    
+import heapq
+class Solution:
+    def caseSort(self,s,n):
+        a = []
+        b = []
+        for k in range(len(s)):
+            if ord(s[k])<97:
+                a.append(ord(s[k]))
+            else:
+                b.append(ord(s[k]))
+        heapq.heapify(a)
+        heapq.heapify(b)
+        a1 = ""
+        for l in range(len(s)):
+            if ord(s[l])>=97:
+                a1 += chr(heapq.heappop(b))
+            else:
+                a1 += chr(heapq.heappop(a))
+        return a1

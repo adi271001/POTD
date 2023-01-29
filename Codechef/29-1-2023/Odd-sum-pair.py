67792e9f0863dafd977be271485c327c41565c19
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-29 12:41:00 +0530

    Create Odd-sum-pair.py

---
+++ Codechef/29-1-2023/Odd-sum-pair.py
@@ -0,0 +1,13 @@
+Question Link: https://www.codechef.com/problems/ODDSUMPAIR
+    
+goals = int(input())
+for distractions in range(goals):
+    wife, life, knife = map(int, input().split())
+    if (wife + life) % 2 == 1:
+        print('YES')
+    elif (wife + knife) % 2 == 1:
+        print('YES')
+    elif (life + knife) % 2 == 1:
+        print('YES')
+    else:
+        print('NO')

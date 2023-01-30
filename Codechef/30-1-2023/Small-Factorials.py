8970c3b8d6b41c05da65f00b493f8ece169f68f1
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-30 11:58:37 +0530

    Create Small-Factorials.py

---
+++ Codechef/30-1-2023/Small-Factorials.py
@@ -0,0 +1,13 @@
+Question Link: https://www.codechef.com/problems/FCTRL2
+    
+def factorial(n):
+    if n==0:
+        return 1
+    elif n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+n=int(input())
+for i in range(n):
+    num=int(input())
+    print(factorial(num))

f3cea5356f32ffbe05703bd8b94045efb91abbc5
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-01-31 23:45:05 +0530

    Create Prefix-Strings.py

---
+++ Newton/Prefix-Strings.py
@@ -0,0 +1,10 @@
+Question Link: https://my.newtonschool.co/playground/code/v1uhocdc7dho
+    
+def prefix(s,t):
+    if(len(x)<=len(y) and x==t[0:len(x)]): 
+        print("Yes")
+    else: 
+        print("No")
+x=input()
+y=input()
+prefix(x,y)

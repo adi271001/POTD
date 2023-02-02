4450bdc2dad8fbf31acc1257a8c3057124d5c368
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-02-02 12:16:36 +0530

    Update Distinct-Coloring.py

--- Gfg/Distinct-Coloring.py
+++ Gfg/Distinct-Coloring.py
@@ -3,13 +3,13 @@ Question Link: https://practice.geeksforgeeks.org/problems/844b4fdcd988ac5461324
 class Solution:
     def distinctColoring (self, N, r, g, b):
         # code here 
-        print(r)
-        print(g)
-        print(b)
-        x=[[0,0,0]]*N
-        x[0] = [r[0],g[0],b[0]]
+        dp = [[0 for i in range(3)] for _ in range(N)]
+        dp[0][0] = r[0]
+        dp[0][1] = g[0]
+        dp[0][2] = b[0]
         for i in range(1,N):
-            x[i][0] = min(x[i-1][1],x[i-1][2])+r[i]
-            x[i][1] = min(x[i-1][0],x[i-1][2])+g[i]
-            x[i][2] = min(x[i-1][0],x[i-1][1])+b[i]
-        return min(x[N-1][0],x[N-1][1],x[N-1][2])
+            dp[i][0] = min(dp[i-1][1],dp[i-1][2]) + r[i]
+            dp[i][1] = min(dp[i-1][0],dp[i-1][2])+g[i]
+            dp[i][2] = min(dp[i-1][0],dp[i-1][1])+b[i]
+            
+        return min(dp[N-1][0],dp[N-1][1],dp[N-1][2])

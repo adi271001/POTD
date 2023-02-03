d63feac4157a98a5d672fab47b20d0336be5343e
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-02-03 16:45:16 +0530

    Update 400M-Race.py

--- Codechef/03-02-2023/400M-Race.py
+++ Codechef/03-02-2023/400M-Race.py
@@ -3,6 +3,6 @@ Question Link: https://www.codechef.com/problems/RACE400M
 for _ in range(int(input())):
     a,b,c = map(int, input().split())
     t = min(a,b,c)
-    if temp == a: print("ALICE")
-    elif temp == b: print("BOB")
+    if t == a: print("ALICE")
+    elif t == b: print("BOB")
     else: print("CHARLIE")

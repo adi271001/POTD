fc29b7aa68412d4912acba514a4c8b8b34302e96
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-02-04 13:22:50 +0530

    Create Chipmunks-and-Nuts.py

---
+++ Newton/Chipmunks-and-Nuts.py
@@ -0,0 +1,9 @@
+Question Link: https://my.newtonschool.co/playground/code/tfp9etbia9hu
+    
+a=int(input())
+b=map(int,input().split())
+s=0
+for k in b:
+    if(k>10):
+        s=s+(k-10)
+print(s)

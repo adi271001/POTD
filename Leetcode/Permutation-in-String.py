13d01cf9d2dee23dda6e8054d64a92168fefeb2d
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-02-04 12:48:11 +0530

    Create Permutation-in-String.py

---
+++ Leetcode/Permutation-in-String.py
@@ -0,0 +1,13 @@
+Question Link: https://leetcode.com/problems/permutation-in-string
+  
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        cntr, w = Counter(s1), len(s1)   
+        for i in range(len(s2)):
+            if s2[i] in cntr: 
+                cntr[s2[i]] -= 1
+            if i >= w and s2[i-w] in cntr: 
+                cntr[s2[i-w]] += 1
+            if all([cntr[i] == 0 for i in cntr]): 
+                return True
+        return False

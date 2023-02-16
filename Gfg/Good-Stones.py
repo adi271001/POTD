03941416c2cc5e3fa3e0ed2006a446047c605e44
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-02-16 12:11:25 +0530

    Create Good-Stones.py

---
+++ Gfg/Good-Stones.py
@@ -0,0 +1,17 @@
+class Solution:
+    def goodStones(self, n, arr) -> int:
+        #code here
+        visited = [0]*n
+        def dfs(i):
+            nonlocal arr, visited, n
+            if i < 0 or i >= n:
+                return True
+                
+            if visited[i] > 0:
+                return visited[i] == 2
+            visited[i] = 1
+            if dfs(i+arr[i]):
+                visited[i] = 2
+                return True
+            return False
+        return sum(dfs(i) for i in range(n))

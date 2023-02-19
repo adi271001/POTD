900e55c8abbad774d7ca8351d28eaafde8c0528d
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-02-19 10:42:07 +0530

    Create Binary-Tree-Zigzag-Level-Order-Traversal.py

---
+++ Gfg/Binary-Tree-Zigzag-Level-Order-Traversal.py
@@ -0,0 +1,32 @@
+Question Link:
+  
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        result = []
+        queue = [root]
+        left_to_right = True
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for i in range(level_size):
+                node = queue.pop(0)
+
+                if left_to_right:
+                    level.append(node.val)
+                else:
+                    level = [node.val] + level
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+            left_to_right = not left_to_right
+
+        return result

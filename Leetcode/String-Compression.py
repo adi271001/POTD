fee1841be31226cea9d351c7498522fc51840b9c
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-03-02 11:03:06 +0530

    Create String-Compression.py

---
+++ Leetcode/String-Compression.py
@@ -0,0 +1,9 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        s = []
+        for key, group in groupby(chars):
+            #print(k, list(g))
+            count = len(list(group))
+            s.append(key)
+            if count > 1: s.extend(list(str(count)))
+        chars[:] = s

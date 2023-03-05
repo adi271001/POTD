fefa0736472e1e0449ba061d60ae4111888e8232
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-03-05 10:45:30 +0530

    Create Find-Index-of-First-Occurence-in--a-string.py

---
+++ Find-Index-of-First-Occurence-in--a-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0
+        elif needle not in haystack:
+            return -1
+        else:
+            return len(haystack.split(needle)[0])

9a7b1c5d7e0e7fcdd03f1fbadfda8a3afb126382
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-04-09 11:21:22 +0530

    Create Sprcial-Digits.py

---
+++ Gfg/Sprcial-Digits.py
@@ -0,0 +1,18 @@
+class Solution:
+    def inverse(self, x, MOD):
+        return pow(x, MOD-2, MOD)
+            
+    def bestNumbers(self, N : int, A : int, B : int, C : int, D : int) -> int:
+        MOD = 10**9 + 7
+        fact = [1]*(N+1)
+        for i in range(1, N+1):
+            fact[i] = (fact[i-1]*i)%MOD
+        
+        res = 0
+        for x in range(N+1):
+            y = N - x
+            S = set(map(int, str(A*x + B*y)))
+            if {C,D} >= S:
+                res = (res + fact[N] * self.inverse(fact[x], MOD) * self.inverse(fact[y], MOD)) % MOD
+        
+        return res

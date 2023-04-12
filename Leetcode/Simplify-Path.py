b67854d75d921a57dae9ee2c730bba1050dbdac2
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-04-12 11:27:22 +0530

    Create Simplify-Path.py

---
+++ Leetcode/Simplify-Path.py
@@ -0,0 +1,10 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        dir_stack = []
+        path = path.split("/")
+        for elem in path:
+            if dir_stack and elem == "..":
+                dir_stack.pop()
+            elif elem not in [".", "", ".."]:
+                dir_stack.append(elem)        
+        return "/" + "/".join(dir_stack)

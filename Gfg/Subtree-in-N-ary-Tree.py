536f1bf79deb4ed7ba917fec575912b09f209476
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-05-01 11:43:23 +0530

    Create Subtree-in-N-ary-Tree.py

---
+++ Gfg/Subtree-in-N-ary-Tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def duplicateSubtreeNaryTree(self, root):
+        #code here
+        from collections import Counter
+        c = Counter()
+        def dfs(n):
+            nonlocal c
+            if not n:
+                return "#"
+            
+            key = "{0} {1}".format(n.key, " ".join(dfs(nxt) for nxt in n.children))
+            c[key] += 1
+            return key
+        dfs(root)
+        return sum(v > 1 for v in c.values())

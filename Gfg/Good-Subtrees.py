8cc8853e1ef070f6b494e677fe708f9f171b4f8b
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-05-05 21:59:27 +0530

    Create Good-Subtrees.py

---
+++ Gfg/Good-Subtrees.py
@@ -0,0 +1,15 @@
+class Solution:
+    def returnTotalGoodSubtrees(self, root, k):
+        if not root:
+            return [0,set()]
+        leftPart = self.returnTotalGoodSubtrees(root.left, k)
+        rightPart = self.returnTotalGoodSubtrees(root.right, k)
+        
+        myset = leftPart[1].union(rightPart[1])
+        myset.add(root.data)
+        goodSubtrees = leftPart[0]+rightPart[0]+1 if len(myset)<=k else leftPart[0]+rightPart[0]
+        return [goodSubtrees, myset]
+    
+    def goodSubtrees(self, root, k):
+        #code here
+        return self.returnTotalGoodSubtrees(root, k)[0]

2103e3d52f1c0127b4db6935abf07da31946aa6c
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-05-29 23:26:03 +0530

    Create Camel-Case-Pattern-Matching.py

---
+++ Gfg/Camel-Case-Pattern-Matching.py
@@ -0,0 +1,15 @@
+class Solution:
+    def CamelCase(self,N,D,P):
+        fn=[]
+        for x in D:
+            ans=""
+            for i in x:
+                if i.isupper()==True:
+                    ans+=i
+                if P==ans[:len(P)]:
+                    fn.append(x)
+                    break
+        if fn :
+            return fn
+        else:
+            return [-1]

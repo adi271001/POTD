0538af76ba1bc262b3c1f859227a53c4b8b128a1
adi271001 <67856422+adi271001@users.noreply.github.com>
2023-06-04 17:13:52 +0530

    Update Problem1-Set-Matrix-Zeros.py

--- Striver-SDE-Sheet-Challenge/Problem1-Set-Matrix-Zeros.py
+++ Striver-SDE-Sheet-Challenge/Problem1-Set-Matrix-Zeros.py
@@ -1,1 +1,18 @@
-
+from math import *
+from collections import *
+from sys import *
+from os import *
+from typing import List
+def setZeros(matrix: List[List[int]]) -> None:
+    rwz = set()
+    cwz = set()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j] == 0:
+                rwz.add(i)
+                cwz.add(j)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if i in rwz or j in cwz:
+                matrix[i][j] = 0
+    return matrix
